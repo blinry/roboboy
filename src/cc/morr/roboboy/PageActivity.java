9bc9b1067f20bfd829af5b6ca542df64e638581d
Sebastian Morr <sebastian@morr.cc>
2013-04-10 13:50:32 +0200

    Dynamic scrolling in PageActivity

--- src/cc/morr/roboboy/PageActivity.java
+++ src/cc/morr/roboboy/PageActivity.java
@@ -9,6 +9,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.widget.EditText;
 import android.widget.Toast;
+import android.widget.ScrollView;
 
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -32,6 +33,8 @@ public class PageActivity extends Activity {
         filename = getDir("wiki", Context.MODE_WORLD_WRITEABLE).getPath()+"/"+pageName;
 
         editText = (EditText)findViewById(R.id.page_text);
+        ScrollView scrollView = (ScrollView)findViewById(R.id.page_scroll_view);
+        scrollView.setSmoothScrollingEnabled(true);
 
         try {
             editText.setText(new Scanner(new File(filename)).useDelimiter("\\Z").next());
@@ -61,7 +64,17 @@ public class PageActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    public void onPause() {
+        super.onPause();
+        save();
+    }
+
     public void save(MenuItem menuItem) {
+        save();
+    }
+
+    public void save() {
         PrintWriter out;
         try {
             out = new PrintWriter(filename);
