67b2010505430b23687bdc64c73f0cf25139a55b
Sebastian Morr <sebastian@morr.cc>
2013-04-10 22:42:41 +0200

    Sync operations in one method again

--- src/cc/morr/roboboy/MainActivity.java
+++ src/cc/morr/roboboy/MainActivity.java
@@ -110,110 +110,58 @@ public class MainActivity extends ListActivity
         }
     }
 
-    private void cloneIt() {
-        try {
-            Git.cloneRepository().setURI(PreferenceManager.getDefaultSharedPreferences(this).getString("repository", "")).setDirectory(new File(localPath)).call();
-        } catch (GitAPIException e) {
-            e.printStackTrace();
-        }
-        Log.d("RoboBoy", "clone done");
-    }
-
-    private void init() {
-        File localRepoDir = new File(localPath + "/.git");
-        if (! localRepoDir.isDirectory()) {
-            Log.d("RoboBoy", "need to clone");
-            cloneIt();
-        }
-        try {
-            localRepo = new FileRepository(localRepoDir);
-        } catch (IOException e) {
-            Log.d("RoboBoy", "repo creation failed");
-            e.printStackTrace();
-        }
-        git = new Git(localRepo);
-
-        try {
-            if (localRepo.getRef("phone") == null)
-                git.checkout().setCreateBranch(true).setName("phone").call();
-            else
-                git.checkout().setName("phone").call();
-        } catch (GitAPIException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        Log.d("RoboBoy", "init done");
-    }
-
-    private void fetch() {
-        try {
-            git.fetch().call();
-        } catch (GitAPIException e) {
-            Log.d("RoboBoy", "fetch failed");
-            e.printStackTrace();
-        }
-        Log.d("RoboBoy", "fetch done");
-    }
-
-    private void merge() {
-        try {
-            git.merge().setFastForward(MergeCommand.FastForwardMode.FF_ONLY).include(localRepo.getRef("origin/master")).call();
-        } catch (CheckoutConflictException e) {
-            runOnUiThread(new Runnable() {
-                public void run() {
-                    Toast.makeText(context, "master on remote diverged, please merge on a real computer.", Toast.LENGTH_LONG).show();
-                }
-            });
-        } catch (GitAPIException e) {
-            Log.d("RoboBoy", "merge failed: git");
-            e.printStackTrace();
-        } catch (IOException e) {
-            Log.d("RoboBoy", "merge failed: io");
-            e.printStackTrace();
-        }
-        Log.d("RoboBoy", "merge done");
-    }
-
-    private void commit() {
-        try {
-            git.add().addFilepattern(".").call();
-        } catch (GitAPIException e) {
-            e.printStackTrace();
-        }
-
-        try {
-            git.commit().setMessage("Sync from RoboBoy").call();
-        } catch (GitAPIException e) {
-            e.printStackTrace();
-        }
-        Log.d("RoboBoy", "commit done");
-    }
-
-    private void push() {
-        try {
-            git.push().add("phone").setForce(true).call();
-        } catch (GitAPIException e) {
-            e.printStackTrace();
-        }
-        Log.d("RoboBoy", "push done");
-    }
-
     private void sync() {
         new Thread(new Runnable() {
             public void run() {
-                init();
-                fetch();
-                merge();
-                commit();
-                push();
-                runOnUiThread(new Runnable() {
-                    public void run() {
-                        Toast.makeText(context, "\"synced\"", Toast.LENGTH_SHORT).show();
-                        listDir(new File(localPath));
+                try {
+                    File localRepoDir = new File(localPath + "/.git");
+                    if (! localRepoDir.isDirectory()) {
+                        Git.cloneRepository().setURI(PreferenceManager.getDefaultSharedPreferences(context).getString("repository", "")).setDirectory(new File(localPath)).call();
                     }
-                });
+
+                    localRepo = new FileRepository(localRepoDir);
+                    git = new Git(localRepo);
+
+                    if (localRepo.getRef("phone") == null)
+                        git.checkout().setCreateBranch(true).setName("phone").call();
+                    else
+                        git.checkout().setName("phone").call();
+
+                    git.fetch().call();
+                    git.merge().setFastForward(MergeCommand.FastForwardMode.FF_ONLY).include(localRepo.getRef("origin/master")).call();
+                    git.add().addFilepattern(".").call();
+                    if (! git.status().call().isClean()) {
+                        git.commit().setMessage("Sync from RoboBoy").call();
+                        git.push().add("phone").setForce(true).call();
+                    }
+
+                    runOnUiThread(new Runnable() {
+                        public void run() {
+                            Toast.makeText(context, "\"synced\"", Toast.LENGTH_SHORT).show();
+                            listDir(new File(localPath));
+                        }
+                    });
+                } catch (CheckoutConflictException e) {
+                    runOnUiThread(new Runnable() {
+                        public void run() {
+                            Toast.makeText(context, "Sync: Conflict error", Toast.LENGTH_LONG).show();
+                        }
+                    });
+                } catch (GitAPIException e) {
+                    e.printStackTrace();
+                    runOnUiThread(new Runnable() {
+                        public void run() {
+                            Toast.makeText(context, "Sync: Git error", Toast.LENGTH_LONG).show();
+                        }
+                    });
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    runOnUiThread(new Runnable() {
+                        public void run() {
+                            Toast.makeText(context, "Sync: IO error", Toast.LENGTH_LONG).show();
+                        }
+                    });
+                }
             }
         }).start();
     }
